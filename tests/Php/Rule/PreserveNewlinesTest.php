bda023f420aaae4d6c76be5928ef39a30c375821
Luke Arms <luke@linacreative.com>
2023-04-04 12:40:21 +1000

    Fix issue where newlines before operators are not always preserved

---
+++ tests/Php/Rule/PreserveNewlinesTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Lkrms\Pretty\Tests\Php\Rule;
+
+final class PreserveNewlinesTest extends \Lkrms\Pretty\Tests\Php\TestCase
+{
+    /**
+     * @dataProvider processTokenProvider
+     */
+    public function testProcessToken(string $code, string $expected)
+    {
+        $this->assertFormatterOutputIs($code, $expected);
+    }
+
+    public static function processTokenProvider()
+    {
+        return [
+            'logical operator after bracket' => [
+                <<<'PHP'
+                <?php
+                return a($b) && a($c)
+                    && strcmp((string) $b, (string) $c) === 0;
+                PHP,
+                <<<'PHP'
+                <?php
+                return a($b) && a($c) &&
+                    strcmp((string) $b, (string) $c) === 0;
+
+                PHP,
+            ],
+        ];
+    }
+}
