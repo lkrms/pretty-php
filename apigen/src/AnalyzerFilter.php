32648f4a4e588aa89db0d86e81b92143e895da28
Luke Arms <luke@linacreative.com>
2023-05-01 16:02:16 +1000

    Replace PhpDocumentor with ApiGen

---
+++ apigen/src/AnalyzerFilter.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Lkrms\ApiGen;
+
+use ApiGen\Analyzer\Filter;
+
+class AnalyzerFilter extends Filter
+{
+}
---
+++ apigen/src/RendererFilter.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Lkrms\ApiGen;
+
+use ApiGen\Renderer\Filter;
+
+class RendererFilter extends Filter
+{
+}
