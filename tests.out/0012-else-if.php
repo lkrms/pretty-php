6235fb6aea8cc72787d484aa8b3ede3dcfd5f4d9
Luke Arms <luke@linacreative.com>
2023-07-05 02:55:02 +1000

    Fix issue where "if" in "else if" is treated as a new statement
    Fixes issue #12

---
+++ tests.out/0012-else-if.php
@@ -0,0 +1,27 @@
+<?php
+if ($a) {
+} else if ($b) {
+} else if ($c) {
+} else {
+}
+if ($a) {
+} else if ($b) {
+} else if ($c) {
+}
+if ($a) {
+    d();
+} else if ($b) {
+    e();
+} else if ($c) {
+    f();
+} else {
+    g();
+}
+if ($a)
+    d();
+else if ($b)
+    e();
+else if ($c)
+    f();
+else
+    g();
