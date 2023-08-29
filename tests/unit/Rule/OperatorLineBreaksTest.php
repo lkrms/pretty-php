1d6593a14ccc2baca07e7d8b49d77127d1de54ed
Luke Arms <luke@linacreative.com>
2023-08-29 15:20:36 +1000

    Fix issue with unaligned method chains
    - If chained method calls would render as below, move the first call in
      the chain to the next line:
    
    ```php
    $foxtrot->foo(
        //
    )
        ->baz();
    ```

---
+++ tests/unit/Rule/OperatorLineBreaksTest.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Lkrms\PrettyPHP\Tests\Rule;
+
+final class OperatorLineBreaksTest extends \Lkrms\PrettyPHP\Tests\TestCase
+{
+    /**
+     * @dataProvider outputProvider
+     */
+    public function testOutput(string $expected, string $code): void
+    {
+        $this->assertCodeFormatIs($expected, $code);
+    }
+
+    /**
+     * @return array<string[]>
+     */
+    public static function outputProvider(): array
+    {
+        return [
+            'chain #1' => [
+                <<<'PHP'
+<?php
+$foxtrot
+    ->foo(
+        fn() =>
+            bar()
+    )
+    ->baz()
+    ->quux();
+
+PHP,
+                <<<'PHP'
+<?php
+$foxtrot->foo(
+fn() =>
+bar()
+)
+->baz()
+->quux();
+PHP,
+            ],
+            'chain #2' => [
+                <<<'PHP'
+<?php
+$foxtrot->foo(fn() =>
+        bar())
+    ->baz()
+    ->quux();
+
+PHP,
+                <<<'PHP'
+<?php
+$foxtrot->foo(fn() =>
+bar()
+)
+->baz()
+->quux();
+PHP,
+            ],
+        ];
+    }
+}
