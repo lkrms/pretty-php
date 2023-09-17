c4f228806255372a12a55a03be514df85c06fb34
Luke Arms <luke@linacreative.com>
2023-09-17 18:33:38 +1000

    Add class comment test fixtures

---
+++ tests/fixtures/out.03-tab/standalone/class-comments-02.php
@@ -0,0 +1,21 @@
+<?php
+class foo
+{
+	function baz()
+	{
+		quux();
+	}  // baz
+}  // foo
+
+// continuation
+class bar
+{
+	function baz()
+	{
+		quux();
+	}  // baz
+}  // bar
+
+// continuation
+class qux {}  // qux
+// continuation
---
+++ tests/fixtures/out.03-tab/standalone/class-comments-03.php
@@ -0,0 +1,56 @@
+<?php
+class foo {}
+class bar {}
+class baz {}
+
+/**
+ *
+ */
+class foo
+{
+	function baz() {}
+}
+
+/**
+ *
+ */
+class bar
+{
+	function baz() {}
+}  /**
+    *
+    */
+
+class qux {}  // qux
+// continuation
+/**
+ *
+ */
+class foo
+{
+	function baz() {}
+}  // foo
+// not a continuation
+
+/**
+ *
+ */
+class bar
+{
+	function baz() {}
+}  /**
+    *
+    */
+
+/**
+ *
+ */
+class qux
+{
+	function foo() {}
+	function bar() {}
+	function baz() {}
+}
+
+class quux {}
+class foo {}
