80c8785bea20fcd27c3498055945576f43cabe6d
Luke Arms <luke@linacreative.com>
2023-10-04 09:25:28 +1100

    Add test fixtures for anonymous functions that return by reference

---
+++ tests/fixtures/out.02-aligned/standalone/functions-anonymous-01.php
@@ -0,0 +1,37 @@
+<?php
+function () {
+    return;
+};
+
+function &()
+{
+    return;
+};
+function (?string $foo,
+          $bar,
+          $baz) {
+    return;
+};
+
+function &(?string $foo,
+    $bar,
+    $baz)
+{
+    return;
+};
+fn &() =>
+    null;
+$foo = function () {
+    return;
+};
+$foo = function &() {
+    return;
+};
+$foo = fn &() =>
+           null;
+fn(?string $foo,
+   $bar,
+   $baz) => null;
+fn &(?string $foo,
+    $bar,
+    $baz) => null;
