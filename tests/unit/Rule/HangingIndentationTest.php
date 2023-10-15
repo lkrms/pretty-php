b062c9de385222572cae495c56c417730e666e37
Luke Arms <luke@linacreative.com>
2023-10-16 08:49:37 +1100

    Fix unreleased regression in hanging indentation

--- tests/unit/Rule/HangingIndentationTest.php
+++ tests/unit/Rule/HangingIndentationTest.php
@@ -103,6 +103,47 @@ PHP,
             [
                 <<<'PHP'
 <?php
+if (
+    $a &&
+    ($b = $c)->d() &&
+    $e->f() &&
+    ($g = h(
+        $i
+    ))->j !== $k &&
+    (($l = $m)->n !== $o ||
+        !(($p = $q) &&
+            $r->s()) ||
+        $t !== $u) &&
+    !($v = $w)->x &&
+    $y < $z
+) {
+    return $a->b;
+}
+
+PHP,
+                <<<'PHP'
+<?php
+if (
+$a &&
+($b = $c)->d() &&
+$e->f() &&
+($g = h(
+$i
+))->j !== $k &&
+(($l = $m)->n !== $o ||
+!(($p = $q) &&
+$r->s()) ||
+$t !== $u) &&
+!($v = $w)->x &&
+$y < $z
+) {
+return $a->b;
+}
+PHP,
+            ],
+            [
+                <<<'PHP'
+<?php
 function a($b, bool $c = false): bool
 {
     return is_array($b) &&
