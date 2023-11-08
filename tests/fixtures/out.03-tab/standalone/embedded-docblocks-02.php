e2015673f9369f25286966011617fb3b5dcf94a9
Luke Arms <luke@linacreative.com>
2023-11-08 13:17:18 +1100

    Add docblock tests

---
+++ tests/fixtures/out.03-tab/standalone/embedded-docblocks-02.php
@@ -0,0 +1,33 @@
+<?php
+$foo = $bar || /*
+                * comment
+                */ $baz;
+
+if ($foo || /*
+             * comment
+             */ $bar) {
+}
+
+function baz(
+	/*
+	 * comment
+	 */
+	$foo,
+
+	/*
+	 * comment
+	 */
+	$bar
+
+	/*
+	 * comment
+	 */
+) {}
+
+class Foo
+{
+	/*
+	 * comment
+	 */
+	use Bar;
+}
