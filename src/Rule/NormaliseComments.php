71a72ca7bb6b15792b7024e90a59c6fabd21b0c0
Luke Arms <luke@linacreative.com>
2024-04-08 11:39:25 +1000

    Improve DocBlock normalisation when leading asterisks are missing
    Input:
    
    ```php
    <?php
    
    /**
     * Comment
     *
     @api
     */
    
    /*
     *
    
    List:
    * Item 1
    * Item 2
    
     */
    ```
    
    Output (before):
    
    ```php
    <?php
    
    /**
     * * Comment
     * *
     * @api
     */
    
    /*
     * List:
     * Item 1
     * Item 2
     */
    ```
    
    Output (after):
    
    ```php
    <?php
    
    /**
     * Comment
     *
     * @api
     */
    
    /*
     * List:
     * * Item 1
     * * Item 2
     */
    ```

--- src/Rule/NormaliseComments.php
+++ src/Rule/NormaliseComments.php
@@ -45,32 +45,68 @@ final class NormaliseComments implements MultiTokenRule
                 $token->id === \T_DOC_COMMENT
                 || ($token->Flags & TokenFlag::INFORMAL_DOC_COMMENT)
             ) {
-                $preserveAsterisk = false;
-                $deindent = null;
-                $asterisksAreMissing = Pcre::match('/\n\h*+(?!\*)\S/', $token->text);
+                $asterisksAreMissing = (bool) Pcre::match('/\n\h*+(?!\*)\S/', $token->text);
                 if ($asterisksAreMissing) {
-                    $firstLineHasContent = Pcre::match(
+                    $deindent = null;
+                    $firstLineHasContent = (bool) Pcre::match(
                         '/^(\/\*++\h*+)(?!\*)\S/',
                         $token->ExpandedText ?? $token->text,
-                        $matches
+                        $matches,
                     );
                     if ($firstLineHasContent) {
-                        $deindent = $token->column - 1 + strlen($matches[1]);
+                        $deindent = $token->column + strlen($matches[1]) - 1;
                     }
 
                     // Get the indent, in spaces, of the character closest to
                     // column 1 that isn't an asterisk
-                    $otherLinesHaveIndent = Pcre::matchAll(
+                    $otherLinesHaveIndent = (bool) Pcre::matchAll(
                         '/^(?!\A)(\h*+)(?!\*)\S/m',
                         $token->ExpandedText ?? $token->text,
                         $matches,
-                        \PREG_SET_ORDER
+                        \PREG_SET_ORDER,
                     );
                     if ($otherLinesHaveIndent) {
                         foreach ($matches as $match) {
                             $length = strlen($match[1]);
                             $deindent = min($deindent ?? $length, $length);
+                            if (!$deindent) {
+                                break;
+                            }
                         }
+                    }
+
+                    // If lines without leading asterisks all start inside the
+                    // margin of the DocBlock, maintain their position unless
+                    // the first character will be deindented, e.g.
+                    //
+                    //     /*
+                    //         if ($foo) {
+                    //             bar();
+                    //         }
+                    //     */
+                    $deindentRegex = $deindent
+                        ? $this->getIndentRegex($deindent)
+                        : '';
+                    if (
+                        $deindent > $token->column + 2
+                        && !Pcre::match(
+                            "/^\/\*++\h*+\\n(?:\h*+(?:\*\h*+)?\\n)*$deindentRegex/",
+                            $token->ExpandedText ?? $token->text,
+                        )
+                    ) {
+                        $deindentRegex = $this->getIndentRegex($token->column + 2);
+                    }
+
+                    // Preserve asterisks aligned with "*" (preferred) or "/" in
+                    // the opening "/*", whichever appears first
+                    $indentRegex = $this->getIndentRegex($token->column);
+                    $altRegex = $this->getIndentRegex($token->column - 1);
+                    if (Pcre::match(
+                        "/\\n(?:$indentRegex|(?<alt>$altRegex))\*/",
+                        $token->ExpandedText ?? $token->text,
+                        $matches,
+                    ) && isset($matches['alt'])) {
+                        $indentRegex = $altRegex;
                     }
 
                     // Expand tabs to spaces if tabs don't appear first in every
@@ -79,21 +115,12 @@ final class NormaliseComments implements MultiTokenRule
                         ? $token->expandedText()
                         : $token->text;
 
-                    if ($deindent) {
-                        $tabs = (int) ($deindent / $this->Formatter->TabSize);
-                        $spaces = $deindent % $this->Formatter->TabSize;
-                        $regex = ($tabs
-                            ? '(?:\t|' . str_repeat(' ', $this->Formatter->TabSize) . "){{$tabs}}"
-                            : '') . str_repeat(' ', $spaces);
-
-                        // If every non-blank inner line will be deindented,
-                        // assume any leading asterisks are part of the content
-                        if (!Pcre::match("/\\n(?!\h*(?:$regex(?!\h)|(\*+\/)?\$))/m", $text)) {
-                            $preserveAsterisk = true;
-                        }
-
-                        $text = Pcre::replace("/\\n(\h*)$regex(?!\h)/", "\n\$1", $text);
-                    }
+                    // Add missing asterisks
+                    $text = Pcre::replace(
+                        "/\\n(?!$indentRegex\*)(\h*)$deindentRegex(?!\h)/",
+                        "\n* \$1",
+                        $text,
+                    );
                 } else {
                     // In comments where every line starts with "*" and ends
                     // with "\h*", remove the trailing asterisks
@@ -117,46 +144,40 @@ final class NormaliseComments implements MultiTokenRule
                     }
                 }
 
-                // Remove comment delimiters and trailing whitespace
-                $replace = ['#^/\*+#', '#\*+/$#', '#\h++$#m'];
-                $with = ['', '', ''];
-
-                // Remove "*" from the start of each line
-                if (!$preserveAsterisk) {
-                    $replace[] = '#\n\h*+\* ?#';
-                    $with[] = "\n";
-                }
-
-                $text = trim(Pcre::replace($replace, $with, $text));
+                // Remove comment delimiters, trailing whitespace, and "*" from
+                // the start of each line
+                $text = trim(Pcre::replace(
+                    ['#^/\*+#', '#\*+/$#', '#\h++$#m', '#\n\h*+\* ?#'],
+                    ['', '', '', "\n"],
+                    $text,
+                ));
 
                 // Collapse DocBlocks with one line of content to a single line
                 // unless they describe a file or are pinned to a declaration
-                if (
-                    ($token->id === \T_DOC_COMMENT
-                        || strpos($token->OriginalText ?? $token->text, "\n") === false)
-                    && strpos($text, "\n") === false
-                    && !($token->NextCode && (
-                        $token->NextCode->id === \T_DECLARE
-                        || $token->NextCode->id === \T_NAMESPACE
-                        || (
-                            $token->NextCode->id === \T_USE
-                            && $token->NextCode->getSubType() === TokenSubType::USE_IMPORT
-                        )
-                    )) && (
-                        !($token->Next
-                            && $token->Next->Statement === $token->Next
-                            && $token->Next->isDeclaration()
-                            && $token->Next->id !== \T_DECLARE)
-                        || ($token->Next->id === \T_USE
-                            && $token->Next->getSubType() === TokenSubType::USE_TRAIT)
+                if ((
+                    ($token->id === \T_DOC_COMMENT && strpos($text, "\n") === false)
+                    || strpos($token->OriginalText ?? $token->text, "\n") === false
+                ) && !($token->NextCode && (
+                    $token->NextCode->id === \T_DECLARE
+                    || $token->NextCode->id === \T_NAMESPACE
+                    || (
+                        $token->NextCode->id === \T_USE
+                        && $token->NextCode->getSubType() === TokenSubType::USE_IMPORT
                     )
-                ) {
+                )) && (!(
+                    $token->Next
+                    && $token->Next->Statement === $token->Next
+                    && $token->Next->isDeclaration()
+                ) || (
+                    $token->Next->id === \T_USE
+                    && $token->Next->getSubType() === TokenSubType::USE_TRAIT
+                ))) {
                     $text = $text === '' ? ' */' : " $text */";
                 } else {
                     $text = preg_replace(
                         ["/\n(?!\n)/", "/\n(?=\n)/"],
                         ["\n * ", "\n *"],
-                        $text
+                        $text,
                     );
                     $text = $text === '' ? "\n *\n */" : "\n * " . $text . "\n */";
                 }
@@ -190,5 +211,14 @@ final class NormaliseComments implements MultiTokenRule
                     break;
             }
         }
+    }
+
+    private function getIndentRegex(int $indent): string
+    {
+        $tabs = (int) ($indent / $this->Formatter->TabSize);
+        $spaces = $indent % $this->Formatter->TabSize;
+        return ($tabs
+            ? '(?:\t|' . str_repeat(' ', $this->Formatter->TabSize) . "){{$tabs}}"
+            : '') . str_repeat(' ', $spaces);
     }
 }
