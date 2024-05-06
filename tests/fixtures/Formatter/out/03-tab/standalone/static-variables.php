ce729901d5ff0e88d4ffe9c14438ee3ee4549856
Luke Arms <luke@linacreative.com>
2024-05-06 12:28:30 +1000

    Add `static-variables.php` to standalone test fixtures

---
+++ tests/fixtures/Formatter/out/03-tab/standalone/static-variables.php
@@ -0,0 +1,26 @@
+<?php
+function foo()
+{
+	static $bar = 0;
+
+	function () use (&$bar) {
+		$bar++;
+	};
+
+	static $baz = 0;
+
+	#[Attr()]
+	function () use (&$baz) {
+		$baz++;
+	};
+
+	static $qux = 0;
+
+	new class($qux)
+	{
+		function __construct(&$qux)
+		{
+			$qux++;
+		}
+	};
+}
